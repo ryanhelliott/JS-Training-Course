ccec8093a6c6d69de2d137ef56ed02ce8b12b171
Ryan Elliott <ryan.elliott@Firehouse.local>
2019-10-23 17:24:45 -0400

    objects and methods

--- script.js
+++ script.js
@@ -506,29 +506,89 @@ In the end, John would like to have 2 arrays:
 // objects - order doesnt matters
 
 // Object Literal
+// var john = {
+//   firstName: 'John',
+//   lastName: 'Smith',
+//   birthYear: 1990,
+//   family: ['Jane', 'Mark', 'Bob', 'Emily'],
+//   job: 'Teacher',
+//   isMarried: false
+// };
+//
+// console.log(john);
+// console.log(john.firstName);
+// console.log(john['lastName']);
+// var x = 'birthYear';
+// console.log(john[x]);
+//
+//
+// john.job = 'designer';
+// john['isMarried'] = true;
+// console.log(john);
+//
+// // New object syntax
+// var jane = new Object();
+// jane.firstName = 'Jane';
+// jane.birthYear = 1969;
+// jane['lastName'] = 'Smith';
+// console.log(jane);
+
+/****************************************
+* Object and Methods
+*/
+
+// Object Literal
+// var john = {
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     birthYear: 1992,
+//     family: ['Jane', 'Mark', 'Bob', 'Emily'],
+//     job: 'Teacher',
+//     isMarried: false,
+//     calcAge: function() {
+//         this.age = 2018 - this.birthYear;
+//       }
+//     };
+//
+//     john.calcAge();
+//     console.log(john);
+//
+//
+//
+
+/****************************************
+* Coding Challenge 4
+*/
+/*
+Lets rememver the first coding challenge where Mark and John compared their BMI's. Lets now implement the same functionality with objects and methods.
+1. For each of them, create an object with properties for their full name, mass, and height.
+2. The, add a method to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
+3. In the end, log to the console who has the highest BMI, together with teh full name and the respective BMI. Dont forget, they might have the same BMI.
+
+remember: BMI = mass / height ^2 = mass / (height * height). mass in kg and height in meters).
+
+*/
+
+
 var john = {
-  firstName: 'John',
-  lastName: 'Smith',
-  birthYear: 1990,
-  family: ['Jane', 'Mark', 'Bob', 'Emily'],
-  job: 'Teacher',
-  isMarried: false
+  fullName: 'John Smith',
+  mass: 10, //kg
+  height: 2, //m
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height * this.height);
+  }
 };
+john.calcBMI();
 
-console.log(john);
-console.log(john.firstName);
-console.log(john['lastName']);
-var x = 'birthYear';
-console.log(john[x]);
+var mark = {
+  fullName: 'Mark Thomson',
+  mass: 5, //kg
+  height: 5, //m
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height * this.height);
+  }
+};
+mark.calcBMI();
 
-
-john.job = 'designer';
-john['isMarried'] = true;
-console.log(john);
-
-// New object syntax
-var jane = new Object();
-jane.firstName = 'Jane';
-jane.birthYear = 1969;
-jane['lastName'] = 'Smith';
-console.log(jane);
+ console.log(john.bmi);
+ console.log(mark.bmi);
